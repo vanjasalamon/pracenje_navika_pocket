628b61b73a457ae63808b633944ef42b428396a1
vanjasalamon <vanjasalamon9@gmail.com>
2025-04-28 12:49:41 +0200

    pocket

---
+++ src/components/AuthProvider.jsx
@@ -0,0 +1,43 @@
+import { createContext, createSignal, useContext, Show, onMount } from "solid-js";
+import { pb } from "../services/pocketbase";
+
+
+//context
+const AuthContext = createContext();
+
+//helper
+export function useAuth() {
+    return useContext(AuthContext);
+}
+
+//component
+export function AuthProvider(props){
+    const [user, setUser] = createSignal(null);
+    const [loading, setLoading] = createSignal(true);
+
+    onMount(async () => {
+        try {
+            const result = await pb.collection("users").authRefresh();
+            const userData = result.record;
+            console.log();
+            setUser(userData);
+        } catch (error) {
+            setUser(null);
+        } finally {
+            setLoading(false);
+        }
+    }
+    );
+
+    pb.authStore.onChange((token, model) => {
+        setUser(model);
+
+    });
+
+return (
+    <Show when={!loading()}>
+        <AuthContext.Provider value={user}>{props.children}</AuthContext.Provider>
+    </Show>
+)};
+
+
